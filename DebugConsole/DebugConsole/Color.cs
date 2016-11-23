98612c6551910b68d3247a5e091ce3ed7d06a80b
crone66 <marcel.croonenbroeck@gmail.com>
2016-11-23 20:30:09 +0100

    added Color struct to replace int array

---
+++ DebugConsole/DebugConsole/Color.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugConsole
+{
+    public struct Color
+    {
+        public byte R;
+        public byte G;
+        public byte B;
+        public byte A;
+
+        public Color(byte r, byte g, byte b)
+        {
+            R = r;
+            G = g;
+            B = b;
+            A = 255;
+        }
+
+        public Color(byte r, byte g, byte b, byte a)
+        {
+            R = r;
+            G = g;
+            B = b;
+            A = a;
+        }
+    }
+}
